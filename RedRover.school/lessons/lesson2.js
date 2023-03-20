081e222ffbe3e970d912e3424ff3a53c162e177e
Svetlana <92315821+Svettlan@users.noreply.github.com>
2023-03-20 17:10:02 +0300

    Add files via upload

---
+++ RedRover.school/lessons/lesson2.js
@@ -0,0 +1,33 @@
+// console.log(5<2 && 3>'3')
+// console.log(5>2 && 3=='3')
+// console.log(8<2 && 3=='3')
+// console.log(7>2 && 3>'3')
+
+// console.log(2>2 || 3<'3')
+// console.log(5<2 || 3=='3')
+// console.log(5!=2 ||  3==='3')
+
+// ================================Задача 1===================================
+let name1
+let age 
+name1 = prompt ("Введите Ваше имя")
+age = prompt("введите Ваш возрат")
+// name1 = "Svetlan"
+// age = 85
+if (age < 21){
+    console.log (`Вот, ${name1}, Ваш стакан сока`);
+    document.write(`Вот, ${name1}, Ваш стакан сока`)
+}
+else if (age>= 21 && age <80){
+    console.log(`Вот, ${name1}, Ваш стакан вина`);
+    document.write(`Вот, ${name1}, Ваш стакан вина`)
+}
+else {
+    console.log (`Вот, ${name1}, Ваш стакан кефира`);
+    document.write(`Вот, ${name1}, Ваш стакан кефира`)
+}
+
+//============================================================= 
+
+document.write(hello)
+    
