5c574c480229fdd64ba06a13bbf713cacdcee2f2
Svetlana <92315821+Svettlan@users.noreply.github.com>
2023-08-28 17:05:09 +0300

    Update script1.js

--- RedRover.school/info/script1.js
+++ RedRover.school/info/script1.js
@@ -101,6 +101,205 @@
 // counter--;        // работает как counter = counter - 1, просто запись короче
 // alert( counter ); // 1
 
+/*========= number -
+- любые числа(целые и дробные, положительные и отрицательные),
+  +Infinity,
+  -Infinity,
+  NaN */
+=========================================================================================================
+=========================================================================================================
+
+  // Математические операции с плавающими числами неточны
+  // console.log(0.1 + 0.2 == 0.3)   //false
+  // console.log(0.1 + 0.2)        // =0.30000000000000004
+
+
+  // let num1 = 1
+  // let num2 = 0
+  // console.log(num1 / num2)  // = Infinity
+  // console.log(-1 / 0)       // = -Infinity
+ 
+  // NaN - this is result of incorrect or undefined mathematical operation:
+  // let firstName = 'Maria'
+  // console.log(firstName / num1)  // = NaN
+  // console.log(firstName - num1)  // = NaN
+  // console.log(firstName * num1)  // = NaN
+
+
+  // console.log(firstName + num1)   // конкатенация => получим строку Maria1
+
+
+  //Но если у нас строка содержит число без букв:
+  // console.log('3' + num1)    // конкатенация => получим строку 31
+  // console.log(num1 + '3')    // конкатенация => получим строку 13
+  // console.log(num1 - '3')    //  => получим число -2
+  // console.log(9 / '3')   // js конвертировал строку в число = 3
+  // console.log('3' * 10)   //то же самое = 30
+
+
+
+
+//=========Cпособы конвертации числа в строку:
+// let num  = 33
+// console.log(num + '')   // 33
+// console.log(typeof(num + ''))   // string
+
+
+// console.log(String(num))   // 33
+// console.log(typeof(String(num)))   // string
+
+
+
+
+//=========Способы конвертации строки в число:
+// let num  = '33'
+// console.log(+num)   // 33
+// console.log(typeof(+num))   // number
+// console.log(num * 1)   // 33
+// console.log(typeof(num * 1))   // number
+
+
+// console.log(Number(num))   // 33
+// console.log(typeof(Number(num)))   // number
+
+
+
+
+
+
+//=========Инкремент/декремент
+//  let counter = 2;
+// counter++        // работает как counter = counter + 1, просто запись короче (или counter += 1)
+// console.log(counter)  // 3
+
+
+// let counter = 2;
+// counter--        // работает как counter = counter - 1, просто запись короче (или counter -= 1)
+// console.log(counter)
+
+
+// Разница между counter++ и ++counter (тоже, что и для counter-- и --counter)
+// «Постфиксная форма»:
+// // counter = 2;
+// console.log(counter++)  // 2
+// console.log(counter + 2)  //5
+
+
+//«Префиксная форма»:
+// counter = 2;
+// console.log(++counter)  // 3
+// console.log(counter)  //3
+
+
+//========= Остаток от деления. Зачем он нам нужен?
+// 1.Проверка числа на четность/нечетность
+// let numOdd = 7
+// let numEven = 8
+// console.log(numOdd % 2 == 0)     //false
+// console.log(numEven % 2 == 0)     //true
+
+
+
+
+// !!!Спрашивают на интервью!!!
+// 2.Проверка, что число целое
+// let numInt = 2
+// let numFloat = 2.2
+// console.log(numFloat % 1 == 0)     //false
+// console.log(numFloat % 1)
+// console.log(numInt % 1 == 0)     //true
+
+
+// // Если находим остаток от деления меньшего числа на большее, то остаток будет всегда первое (меньшее) число
+// console.log(1 % 2)
+
+
+// !!!Спрашивают на интервью!!!
+//========= Операторы сравнения
+// В чем отличие строгого оператора сравнения/strict equallity (===) от нестрогого/ loose equallity (==)?
+
+
+// Например, '==' не отличает 0 от false
+// console.log(0 == false)   // true
+// console.log(0 === false)   // false, так как различны их типы
+
+
+// '==' не сравнивает типы
+// console.log('3' == 3)        // true
+// console.log('3' === 3)        // false, так как различны их типы
+
+
+// При использовании математических операторов и других операторов сравнения < > <= >=
+// Значения null/undefined преобразуются к числам: null становится 0, а undefined – NaN.
+// console.log( null > 0 )    // 0 > 0 ? false
+// console.log( null >= 0 )   // 0 >= 0 ? true
+// console.log(null - 5)  // -5, здесь null это 0
+
+
+// для нестрогого равенства == значений undefined и null действует особое правило:
+// эти значения ни к чему не приводятся, они равны друг другу и не равны ничему другому.
+// console.log(null == undefined)       // true
+// console.log(null == 0)                  // false
+// console.log(undefined == NaN)                  // false
+
+
+// для строгого равенства === Эти значения различны, так как различны их типы.
+// console.log(null === undefined)      // false
+
+
+
+
+//=========Logical operators
+
+
+// || (Оператор «ИЛИ»)
+
+
+// result = value1 || value2 || value3
+// Вычисляет операнды слева направо.
+// Каждый операнд конвертирует в логическое значение. Если результат true, останавливается и возвращает исходное значение этого операнда
+// Другими словами, цепочка ИЛИ || возвращает первое истинное значение или последнее, если такое значение не найдено
+
+
+// Пример
+// let firstName = ""
+// let lastName = ""
+// let nickName = "Суперкодер"
+
+
+// ИЛИ || обрабатывает свои операнды до тех пор, пока не будет достигнуто первое истинностное значение,
+// и затем это значение сразу же возвращается, даже не затрагивая другие операнды.
+
+
+// console.log(firstName || lastName || nickName || "Аноним")   //Суперкодер
+// console.log(!firstName)   //true
+
+
+// console.log(true || console.log("никогда не сработает"))
+// console.log(false || console.log("сработает"))
+
+
+// && (Оператор «И»)
+// находит первое ложное значение
+
+
+// result = value1 && value2 && value3
+// Вычисляет операнды слева направо.
+// Каждый операнд преобразует в логическое значение. Если результат false, останавливается и возвращает исходное значение этого операнда.
+// Если все операнды были истинными, возвращается последний.
+
+
+// Пример
+// console.log(1 && 2 && 0 && 3)  // 0
+// console.log(!0)                // true
+// console.log(!!0)                // false
+// console.log(0 && "какая-то строка")   //0
+// console.log(1 && "какая-то строка")   //"какая-то строка"
+
+
+// Приоритет оператора && больше, чем у ||
+// console.log(true || true && false);    // true
+// console.log((true || true) && false);  // false
 
 
 
